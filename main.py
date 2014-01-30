c0154d2877b8456937913a139857ef336ef3a476
Kory <kory75@gmail.com>
2014-01-30 17:02:06 +0000

    scanning and file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import kivy
 from kivy.app import App
 #from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.camera import Camera
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty            
 
 class MenuScreen(Screen):
     pass
